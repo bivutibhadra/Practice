d0a46ca034d279c44127681426d4a33a821b981d
Bivuti Bhadra <bivuti001@gmail.com>
2021-04-28 19:21:39 +0600

    Update input key

--- src/inheritence/Teacher.java
+++ src/inheritence/Teacher.java
@@ -3,11 +3,8 @@ package inheritence;
 public class Teacher extends Personn {
         String quali;
            void displayInformation2(){
-<<<<<<< HEAD
-              displayInformation();
-=======
+
                displayInformation();
->>>>>>> 1606308302066aabc02d5801279bb038089d6fbd
                System.out.println("quli = "+quali);
                System.out.println("");
 
--- src/inheritence/Test.java
+++ src/inheritence/Test.java
@@ -1,18 +1,47 @@
 package inheritence;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner inputdata = new Scanner(System.in);
+       // Scanner inputdata = Scanner(System.in);
+
         Teacher t1 = new Teacher();
         Teacher t2 = new Teacher();
 
-        t1.name = "Bivuti";
-        t1.age = 30;
-        t1.quali = "Bsc";
+
+        System.out.print("Enter your Name = ");
+        t1.name= inputdata.next();
+
+        System.out.print("Enter your Age = ");
+        t1.age = inputdata.nextInt();
+
+        System.out.print("Enter your Qua = ");
+        t1.quali = inputdata.next();
+        System.out.println(" ");
+
         t1.displayInformation2();
 
-        t2.name = "Hashi";
-        t2.age = 25;
-        t2.quali = "Bsc";
+
+        System.out.println("  ");
+
+        //Teachers 2
+
+        System.out.print("Enter your Name = ");
+        t2.name= inputdata.next();
+
+        System.out.print("Enter your Age = ");
+        t2.age = inputdata.nextInt();
+
+        System.out.print("Enter your Qua = ");
+        t2.quali = inputdata.next();
+        System.out.println(" ");
+
+
         t2.displayInformation2();
+
     }
+
 }
+
