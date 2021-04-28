2ec55e1c61a40516d26b387baca1f0c4a524f4ab
Bivuti Bhadra <bivuti001@gmail.com>
2021-04-28 14:30:36 +0600

    update inheritence

--- src/inheritence/Teacher.java
+++ src/inheritence/Teacher.java
@@ -3,9 +3,10 @@ package inheritence;
 public class Teacher extends Personn {
         String quali;
            void displayInformation2(){
-               System.out.println("Name = "+name);
-               System.out.println("age = "+age);
+              displayInformation();
                System.out.println("quli = "+quali);
+               System.out.println("");
+
 
            }
     }
--- src/inheritence/Test.java
+++ src/inheritence/Test.java
@@ -3,9 +3,16 @@ package inheritence;
 public class Test {
     public static void main(String[] args) {
         Teacher t1 = new Teacher();
+        Teacher t2 = new Teacher();
+
         t1.name = "Bivuti";
         t1.age = 30;
         t1.quali = "Bsc";
         t1.displayInformation2();
+
+        t2.name = "Hashi";
+        t2.age = 25;
+        t2.quali = "Bsc";
+        t2.displayInformation2();
     }
 }
