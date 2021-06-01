aba628aef5f939dad1f684389e242b772302f8fb
Bivuti <bivuti001@gmail.com>
2021-06-01 20:50:32 +0600

    encapulation

---
+++ src/encapsulation/EncriptTest.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class EncriptTest {
+    public static void main(String[] args) {
+        Persion ob = new Persion();
+ob.setName("Bivuti");
+ob.setAge(27);
+
+        System.out.println("Name = "+ob.getName());
+        System.out.println("Age = "+ob.getAge());
+
+    }
+}
