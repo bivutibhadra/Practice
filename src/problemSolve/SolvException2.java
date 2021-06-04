422c525d4ab7e2869aac328385a3a38095669277
Bivuti <bivuti001@gmail.com>
2021-06-04 21:38:36 +0600

    update

--- src/problemSolve/SolvException2.java
+++ src/problemSolve/SolvException2.java
@@ -6,7 +6,7 @@ public class SolvException2 {
     public static void main(String[] args) {
 
 
-        int count = 1;
+        int count = 1;           //question solve
         do {
 
             try {
