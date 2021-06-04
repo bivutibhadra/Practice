47c4acacc2f51f339b31bde9b62f88b2d0767d4e
Bivuti <bivuti001@gmail.com>
2021-06-04 19:59:09 +0600

    Exception question solv

---
+++ src/excepTest/Main.java
@@ -0,0 +1,13 @@
+package excepTest;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = divide (a,b);
+        System.out.println("Result = "+result);
+    }
+    public static int divide(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/problemSolve/SolvException2.java
@@ -0,0 +1,35 @@
+package problemSolve;
+
+import java.util.Scanner;
+
+public class SolvException2 {
+    public static void main(String[] args) {
+
+
+        int count = 1;
+        do {
+
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Please enter num1 : ");
+                int num1 = input.nextInt();
+
+                System.out.print("Please enter num2 : ");
+                int num2 = input.nextInt();
+
+                int result = num1 / num2;
+                System.out.println("Result :" + num1 + "/" + num2 + "=" + result);
+                count = 2;
+            } catch (Exception e) {
+
+                System.out.println("Exception :" + e);
+                System.out.println("You much enter integer.Please try again....");
+
+            }
+        } while (count == 1);
+
+
+    }
+
+
+}
