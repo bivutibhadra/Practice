4a95ad6d56bc754251e545e93bccdeb56ce21df3
Bivuti <bivuti001@gmail.com>
2021-06-20 20:34:53 +0600

    Book example

---
+++ src/bookExmple2/Exmple.java
@@ -0,0 +1,20 @@
+package bookExmple2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exmple {
+
+    public static void main(String[] args) throws IOException {
+       BufferedReader reader;
+       reader =new BufferedReader(new InputStreamReader(System.in));
+       String line;
+       do {
+           line = reader.readLine();
+           line = line.toUpperCase();
+           System.out.println(line);
+       }while(!"quit".equals(line));
+    }
+
+}
