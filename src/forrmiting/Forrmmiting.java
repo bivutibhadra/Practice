a71bb5a528d71610b38b5338dd5ddc513bdcb986
Bivuti <bivuti001@gmail.com>
2021-06-25 22:48:57 +0600

    formatting

---
+++ src/forrmiting/Forrmmiting.java
@@ -0,0 +1,14 @@
+package forrmiting;
+
+import java.text.DecimalFormat;
+
+public class Forrmmiting {
+    public static void main(String[] args) {
+
+
+    DecimalFormat ob =new DecimalFormat("0.000");
+    double x = 2.123845;
+        System.out.println("Format " +ob.format (x));
+
+    }
+}
