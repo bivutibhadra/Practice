caaf73e3aa5eefd1bf3a5365a1ce6103a2491621
Bivuti <bivuti001@gmail.com>
2021-06-27 17:18:04 +0600

    tostring

---
+++ src/toString/Person.java
@@ -0,0 +1,20 @@
+package toString;
+
+public class Person {
+
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/toString/PersonTest.java
@@ -0,0 +1,13 @@
+package toString;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person("Bivuti", 27);
+        Person p2 = new Person("Hashi", 21);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+
+    }
+}
